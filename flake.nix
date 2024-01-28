4f6ca079bccdedbe5537eccc12d6b9f70ac8efed
chaseunc0 <radioaddition@murena.io>
2024-01-28 11:12:44 -0500

    combined home-manager and default flake into one file:   flake.nix 	deleted:    home-manager/flake.lock 	deleted:    home-manager/flake.nix

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  # what will be produced (i.e. the build)
-  outputs = {self, nixpkgs, unstable}@inputs:
+  outputs = {self, nixpkgs, home-manager, unstable, ... }@inputs:
   {
     nixosConfigurations = {
       galith = nixpkgs.lib.nixosSystem {
@@ -24,6 +24,24 @@
         modules = [
           ./hosts/aspirem.nix
           ./hosts/aspirem-hardware.nix
+        ];
+      };
+    };
+      homeConfigurations = {
+      "aspirem" = home-manager.lib.homeManagerConfiguration {
+	    extraSpecialArgs = {inherit inputs;};
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home-manager/hosts/aspirem.nix
+        ];
+      };
+      "galith" = home-manager.lib.homeManagerConfiguration {
+	    extraSpecialArgs = {inherit inputs;};
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          ./home-manager/hosts/galith.nix
         ];
       };
     };
