0828cfd641fdeb31514a859357cfd7b380ae167b
RadioAddition <radioaddition@murena.io>
2024-02-04 18:25:20 +0000

    modified:   flake.lock 	added nix-on-droid:   flake.nix 	new file:   hosts/pixel6.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    oldpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     home-manager.url = "github:nix-community/home-manager";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-on-droid = {
+      url = "github:t184256/nix-on-droid/release-23.05";
+      inputs.nixpkgs.follows = "oldpkgs";
+    };
   };
 
-  outputs = {self, nixpkgs, home-manager, unstable, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, unstable, oldpkgs, nix-on-droid, ... }@inputs:
   {
     nixosConfigurations = {
       galith = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	    specialArgs = {inherit inputs;};
+	specialArgs = {inherit inputs;};
         modules = [
           ./hosts/galith.nix
           ./hosts/galith-hardware.nix
@@ -32,6 +37,12 @@
 	modules = [
 	  ./containers/wordpress.nix
 	];
+      };
+    };
+    nixOnDroidConfigurations = {
+      pixel6 = nix-on-droid.lib.nixOnDroidConfiguration {
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./hosts/pixel6.nix ];
       };
     };
       homeConfigurations = {
