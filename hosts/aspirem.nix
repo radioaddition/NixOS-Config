8a7b4295b358f620108f9711cb1f8fbee8d0aea6
AadioAddition <radioaddition@murena.io>
2024-07-23 04:45:42 -0400

    modified:   hosts/aspirem.nix

--- hosts/aspirem.nix
+++ hosts/aspirem.nix
@@ -53,11 +53,6 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  # Enable Hyprland
-  programs.hyprland.enable = true;
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     xkb = {
