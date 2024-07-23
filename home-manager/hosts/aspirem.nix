4862c776f124d19d1be709194b218439bcc9e1c4
AadioAddition <radioaddition@murena.io>
2024-07-23 03:45:44 -0400

    modified:   home-manager/hosts/aspirem.nix

--- home-manager/hosts/aspirem.nix
+++ home-manager/hosts/aspirem.nix
@@ -57,20 +57,8 @@
 	"sindresorhus/pure"
       ];
     };
-    initExtraFirst = ''
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
-      clear
-    '';
     initExtra = ''
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-
-. ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      source /etc/nixos/home-manager/.p10k.zsh
 '';
   };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
