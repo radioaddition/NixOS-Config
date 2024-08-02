5b70714cc23417a5af0fce28a7610a9fae6c3e1b
AadioAddition <radioaddition@murena.io>
2024-08-02 01:37:39 -0400

    modified:   home-manager/hosts/aspirem.nix

--- home-manager/hosts/aspirem.nix
+++ home-manager/hosts/aspirem.nix
@@ -59,8 +59,6 @@
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
-      clear
-      hyfetch
     '';
     initExtra = ''
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
