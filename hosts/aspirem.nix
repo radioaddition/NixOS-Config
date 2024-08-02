31454ae9a6f199c0f1c33a4e1ff782e45682ecce
AadioAddition <radioaddition@murena.io>
2024-08-02 01:15:07 -0400

    modified:   hosts/aspirem.nix

--- hosts/aspirem.nix
+++ hosts/aspirem.nix
@@ -9,6 +9,19 @@
 
   ## Security
 
+  ### Auto Updates
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--no-write-lock-file"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
   ### AppArmor
   services.dbus.apparmor = "enabled";
   security.apparmor = {
