0cfe8a5b316556772b3978b7fd07890c1afe37e0
RadioAddition <radioaddition@pm.me>
2025-01-17 00:05:46 -0500

    Removed old and unused devices

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-on-droid.url = "github:t184256/nix-on-droid/release-24.05";
     fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.21.tar.gz";
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v0.4.1";
@@ -14,22 +13,8 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, lanzaboote, home-manager, unstable, nix-on-droid, fh, ... }: {
+  outputs = inputs@{ self, nixpkgs, lanzaboote, home-manager, unstable, fh, ... }: {
     nixosConfigurations = {
-      aspirem = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
-        modules = [
-	  lanzaboote.nixosModules.lanzaboote
-          ./hosts/aspirem/configuration.nix
-          ./hosts/aspirem/hardware-configuration.nix
-	  home-manager.nixosModules.home-manager {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.radioaddition = [ import ./hosts/aspirem/home.nix ];
-          }
-	];
-      };
       galith = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -44,13 +29,6 @@
         ];
       };
     };
-    nixOnDroidConfigurations = {
-      "oriole" = nix-on-droid.lib.nixOnDroidConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-linux"; };
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./hosts/oriole/configuration.nix ];
-      };
-    };
     homeConfigurations = {
       "aspirem" = home-manager.lib.homeManagerConfiguration {
         extraSpecialArgs = {inherit inputs;};
@@ -59,26 +37,11 @@
           ./hosts/aspirem/home.nix
         ];
       };
-      "aspirem-secureblue" = home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = {inherit inputs;};
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [
-          ./hosts/aspirem-secureblue/home.nix
-        ];
-      };
-      "oriole" = home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = {inherit inputs;};
-        pkgs = nixpkgs.legacyPackages."aarch64-linux";
-        modules = [
-          ./hosts/oriole/home.nix
-        ];
-      };
       "framework" = home-manager.lib.homeManagerConfiguration {
   	extraSpecialArgs = {inherit inputs;};
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
           ./hosts/framework/home.nix
-          ./hosts/framework/nushell.nix
         ];
       };
       "galith" = home-manager.lib.homeManagerConfiguration {
@@ -93,13 +56,6 @@
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [
           ./hosts/deck/home.nix
-        ];
-      };
-      "air2020" = home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = {inherit inputs;};
-        pkgs = nixpkgs.legacyPackages."x86_64-darwin";
-        modules = [
-          ./hosts/air2020/home.nix
         ];
       };
     };
