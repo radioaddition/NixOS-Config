1706b87db5c55f04974762745de812a26fa17c9c
RadioAddition <radioaddition@pm.me>
2025-01-17 16:34:14 -0500

    new file:   base-gnome.nix 	renamed:    home.nix -> base-home.nix 	new file:   shells/zsh.nix

---
+++ base/base-home.nix
@@ -0,0 +1,106 @@
+{ pkgs, config, libs, inputs, ... }:
+
+{
+  
+  # in case of git.sr.ht outage
+  #manual.html.enable = false;
+  #manual.manpages.enable = false;
+  #manual.json.enable = false;
+
+
+  imports = [
+  ];
+  #news.display = "silent";
+  # Commented out because this can occasionally be different (ie some of my hosts have multiple users, and some are on Fedora/RHEL based (which uses /var/home)) but I need to be reminded to set it per host
+  #home.username = "radioaddition";
+  #home.homeDirectory = "/home/radioaddition";
+
+  nixpkgs.config.allowUnfree = true;
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    DBX_CONTAINER_MANAGER = "podman";
+  };
+  # GPG
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
+  };
+
+  # Direnv
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
+
+  # ZSH
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    warn-dirty = false;
+  };
+  nix.package = pkgs.nixFlakes;
+  home.packages = with pkgs; [
+
+  # Packages
+    atuin
+    bat
+    btop
+    chezmoi
+    curl
+    direnv
+    eza
+    freshfetch
+    gh
+    git-repo
+    glas
+    gleam
+    home-manager
+    hyfetch
+    iosevka
+    just
+    meslo-lgs-nf
+    mpv
+    neovim
+    nodePackages_latest.pnpm
+    pipx
+    pnpm-shell-completion
+    python3
+    starship
+    topgrade
+    usbtop
+#    docker-compose
+#    gcc
+#    gettext
+#    git
+#    github-desktop
+#    glib
+#    gnumake
+#    gparted
+#    guake
+#    jamesdsp
+#    llama-cpp
+#    mindustry-wayland
+#    monophony
+#    neovim-gtk
+#    nodejs-slim
+#    onionshare-gui
+#    openrazer-daemon
+#    pavucontrol
+#    perl
+#    pinentry-gnome3
+#    polychromatic
+#    protonvpn-gui
+#    redis
+#    shattered-pixel-dungeon
+#    wget
+#    wl-clipboard
+#    wlrctl
+#    xmrig-mo
+
+    ];
+
+  #home.enableNixpkgsReleaseCheck = false; # If using a package from the unstable branch uncomment this
+  home.stateVersion = "24.05";
+}
---
+++ base/shells/zsh.nix
@@ -0,0 +1,63 @@
+{ pkgs, config, libs, inputs, ... }: {
+
+  home.sessionPath = [ "$HOME/.local/bin" "$HOME/bin" "$HOME/.cargo/bin" "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/var/home/linuxbrew/.linuxbrew/Cellar/zplug/2.4.2/bin" "$HOME/.nix-profile/bin" ];
+  programs.zsh = {
+    enable = true;
+    # Zprof will profile your zsh startup time
+    # zprof.enable = true;
+    shellAliases = {
+      clearls = "clear && ls -A";
+      archive = "tar -czvf archive.tar.gz ";
+      extract = "tar -xzvf ";
+      vivi = "nvim /home/radioaddition/.config/nvim/init.vim";
+      clean = "nix-env --delete-generations old && nix-collect-garbage -d && nix profile wipe-history";
+      cleanr = "run0 sh -c 'nix-env --delete-generations old && nix-collect-garbage -d && nix profile wipe-history'";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "jeffreytse/zsh-vi-mode"; }
+      ];
+    };
+    history = {
+      ignoreAllDups = true;
+      extended = true;
+    };
+    initExtra = ''
+. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+      eval "$(atuin init zsh)"
+      eval "$(direnv hook zsh)"
+      eval "$(starship init zsh)"
+      eval "$(chezmoi completion zsh)"
+      eval "$(just --completions zsh)"
+'';
+  };
+
+  # Atuin
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      update_check = false;
+      filter_mode = "session";
+      workspaces = true;
+      style = "auto";
+      exit_mode = "return-query";
+      ctrl_n_shortcuts = false;
+      enter_accept = true;
+      keymap_mode = "vim-insert";
+      keymap_cursor = {
+        emacs = "blink-block";
+	vim_insert = "blink-line";
+	vim_normal = "steady-block";
+      };
+    };
+  };
+  # Direnv
+  # Zoxide
+  # Starship
+  # Chezmoi?
+}
