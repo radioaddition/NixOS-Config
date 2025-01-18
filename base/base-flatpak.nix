8ccaeb525dbe242b80fc3d6d8593d8d0dc0704b3
RadioAddition <radioaddition@pm.me>
2025-01-17 20:18:51 -0500

    fix flatpak remotes config

--- base/base-flatpak.nix
+++ base/base-flatpak.nix
@@ -9,8 +9,8 @@
       onCalendar = "daily";
     };
     remotes = [
-      { name = "flathub-verified"; subset = "verified"; location = "https://flathub.org/repo/flathub.flatpakrepo"; };
-      { name = "flathub"; location = "https://flathub.org/repo/flathub.flatpakrepo"; };
+      { name = "flathub-verified"; subset = "verified"; location = "https://flathub.org/repo/flathub.flatpakrepo"; }
+      { name = "flathub"; location = "https://flathub.org/repo/flathub.flatpakrepo"; }
     ];
     packages = [
     # Set either with just the appID in quotes ("com.app.app") or with the app/remote/etc in {} ({ appId = "com.app.app"; origin = "remote";  })
