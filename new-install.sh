819c54cdf71bd73965e577512ab1508ae5376f21
RadioAddition <radioaddition@pm.me>
2025-01-19 17:47:09 -0500

    improve install script

--- new-install.sh
+++ new-install.sh
@@ -3,4 +3,8 @@ if [ "$2" == "" ]; then
 	echo Usage: ./new-install.sh [hostname] [disk device]
 	exit 1
 fi
+nixos-generate-config --root /tmp --no-filesystems
+mv /tmp/etc/nixos/hardware-configuration.nix ./hosts/$1/
+git add ./hosts/$1/hardware-configuration.nix
+rm -rf /tmp/etc
 sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#"$1" --disk main "$2"
