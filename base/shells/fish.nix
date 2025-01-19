4d5d72d2315ffa1597891711cac2d6c89fe5e240
RadioAddition <radioaddition@pm.me>
2025-01-19 12:32:33 -0500

    fixed fish colors (FINALLY)

--- base/shells/fish.nix
+++ base/shells/fish.nix
@@ -1,6 +1,6 @@
 { pkgs, config, libs, inputs, ... }: {
 
-  home.sessionPath = [ "$HOME/.local/bin" "$HOME/bin" "$HOME/.cargo/bin" "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/var/home/linuxbrew/.linuxbrew/Cellar/zplug/2.4.2/bin" "$HOME/.nix-profile/bin" ];
+  home.sessionPath = [ "$HOME/.local/bin" "$HOME/bin" "$HOME/.cargo/bin" "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "$HOME/.nix-profile/bin" ];
   programs.fish = {
     enable = true;
     shellAliases = {
@@ -12,58 +12,60 @@
       cleanr = "run0 sh -c 'nix-env --delete-generations old && nix-collect-garbage -d && nix profile wipe-history'";
       so = "exec fish";
     };
-    shellInit = ''
-# Apply home.sessionPath and home.sessionVariables if set
-. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    interactiveShellInit = ''
 
-      # Colors (copy + pasted from the webui output)
-      SETUVAR __fish_initialized:3400
-      SETUVAR fish_color_autosuggestion:666
-      SETUVAR fish_color_cancel:\x2d\x2dreverse
-      SETUVAR fish_color_command:00ff00
-      SETUVAR fish_color_comment:888\x1e\x2d\x2ditalics
-      SETUVAR fish_color_cwd:0A0
-      SETUVAR fish_color_cwd_root:A00
-      SETUVAR fish_color_end:ffff00
-      SETUVAR fish_color_error:F22
-      SETUVAR fish_color_escape:0AA
-      SETUVAR fish_color_history_current:0AA
-      SETUVAR fish_color_host:normal
-      SETUVAR fish_color_host_remote:\x1d
-      SETUVAR fish_color_keyword:\x1d
-      SETUVAR fish_color_match:0AA
-      SETUVAR fish_color_normal:B2B2B2
-      SETUVAR fish_color_operator:0AA
-      SETUVAR fish_color_option:\x1d
-      SETUVAR fish_color_param:0087d7
-      SETUVAR fish_color_quote:ffd75f
-      SETUVAR fish_color_redirection:00ffd7
-      SETUVAR fish_color_search_match:\x2d\x2dbackground\x3d533
-      SETUVAR fish_color_selection:\x2d\x2dbackground\x3dB218B2
-      SETUVAR fish_color_status:red
-      SETUVAR fish_color_user:brgreen
-      SETUVAR fish_color_valid_path:\x2d\x2dunderline
-      SETUVAR fish_key_bindings:fish_default_key_bindings
-      SETUVAR fish_pager_color_background:\x1d
-      SETUVAR fish_pager_color_completion:BBB
-      SETUVAR fish_pager_color_description:666
-      SETUVAR fish_pager_color_prefix:0AA\x1e\x2d\x2dunderline
-      SETUVAR fish_pager_color_progress:0AA
-      SETUVAR fish_pager_color_secondary_background:\x1d
-      SETUVAR fish_pager_color_secondary_completion:\x1d
-      SETUVAR fish_pager_color_secondary_description:\x1d
-      SETUVAR fish_pager_color_secondary_prefix:\x1d
-      SETUVAR fish_pager_color_selected_background:\x2d\x2dbackground\x3d333
-      SETUVAR fish_pager_color_selected_completion:\x1d
-      SETUVAR fish_pager_color_selected_description:\x1d
-      SETUVAR fish_pager_color_selected_prefix:\x1d
+    # Colors
+    set -Ux fish_initialized 3400
+    set -Ux fish_color_autosuggestion 666
+    set -Ux fish_color_cancel \x2d\x2dreverse
+    set -Ux fish_color_command 00ff00
+    set -Ux fish_color_comment 888\x1e\x2d\x2ditalics
+    set -Ux fish_color_cwd 0A0
+    set -Ux fish_color_cwd_root A00
+    set -Ux fish_color_end ffff00
+    set -Ux fish_color_error F22
+    set -Ux fish_color_escape 0AA
+    set -Ux fish_color_history_current 0AA
+    set -Ux fish_color_host normal
+    set -Ux fish_color_host_remote \x1d
+    set -Ux fish_color_keyword \x1d
+    set -Ux fish_color_match 0AA
+    set -Ux fish_color_normal B2B2B2
+    set -Ux fish_color_operator 0AA
+    set -Ux fish_color_option \x1d
+    set -Ux fish_color_param 0087d7
+    set -Ux fish_color_quote ffd75f
+    set -Ux fish_color_redirection 00ffd7
+    set -Ux fish_color_search_match \x2d\x2dbackground\x3d533
+    set -Ux fish_color_selection \x2d\x2dbackground\x3dB218B2
+    set -Ux fish_color_status red
+    set -Ux fish_color_user brgreen
+    set -Ux fish_color_valid_path \x2d\x2dunderline
+    set -Ux fish_key_bindings fish_default_key_bindings
+    set -Ux fish_pager_color_background \x1d
+    set -Ux fish_pager_color_completion BBB
+    set -Ux fish_pager_color_description 666
+    set -Ux fish_pager_color_prefix 0AA\x1e\x2d\x2dunderline
+    set -Ux fish_pager_color_progress 0AA
+    set -Ux fish_pager_color_secondary_background \x1d
+    set -Ux fish_pager_color_secondary_completion \x1d
+    set -Ux fish_pager_color_secondary_description \x1d
+    set -Ux fish_pager_color_secondary_prefix \x1d
+    set -Ux fish_pager_color_selected_background \x2d\x2dbackground\x3d333
+    set -Ux fish_pager_color_selected_completion \x1d
+    set -Ux fish_pager_color_selected_description \x1d
+    set -Ux fish_pager_color_selected_prefix \x1d
 
       # shell inits
-      eval $(atuin init fish)
-      eval $(direnv hook fish)
-      eval $(starship init fish)
-      eval $(chezmoi completion fish)
-      eval $(just --completions fish)
+      function starship_transient_prompt_func
+        starship module character
+      end
+      starship init fish | source
+      enable_transience
+      atuin init fish | source
+      direnv hook fish | source
+      chezmoi completion fish | source
+      just --completions fish | source
 '';
   };
   programs.zoxide.enable = true;
