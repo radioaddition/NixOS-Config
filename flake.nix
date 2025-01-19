865dd4c507a97f779d059aef062cf1e6e032f603
RadioAddition <radioaddition@pm.me>
2025-01-19 17:03:50 -0500

    finish previous commit

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, lanzaboote, home-manager, unstable, nix-flatpak, ... }: {
+  outputs = inputs@{ self, nixpkgs, lanzaboote, home-manager, unstable, nix-flatpak, disko, impermanence, ... }: {
     nixosConfigurations = {
       framework = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
