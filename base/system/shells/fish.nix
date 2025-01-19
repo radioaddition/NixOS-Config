1c41c69b7bff48591cd33bdc9dd4bc82505f16fa
RadioAddition <radioaddition@pm.me>
2025-01-19 12:52:02 -0500

    add babelfish to the right place this time

--- base/system/shells/fish.nix
+++ base/system/shells/fish.nix
@@ -1,6 +1,7 @@
 { config, pkgs, inputs, lib, ... }: {
   ## Enable fish
   programs.fish.enable = true;
+  programs.fish.useBabelfish = true;
   users.defaultUserShell = pkgs.fish;
   environment.shells = [ pkgs.fish ];
 }
