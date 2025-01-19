c5413a72124373dd16c4d666281bd94072262111
RadioAddition <radioaddition@pm.me>
2025-01-19 16:05:54 -0500

    add script for new installs using disko

---
+++ new-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [ "$2" == "" ]; then
+	echo Usage: ./new-install.sh -- [hostname] [disk device]
+	exit 1
+fi
+sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#"$1" --disk main "$2"
