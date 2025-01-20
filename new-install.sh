f25b19d247e87b9721774014448dd871f2728e13
RadioAddition <radioaddition@pm.me>
2025-01-19 19:23:02 -0500

    revert last change to install script

--- new-install.sh
+++ new-install.sh
@@ -3,6 +3,8 @@ if [ "$2" == "" ]; then
 	echo Usage: ./new-install.sh [hostname] [disk device]
 	exit 1
 fi
-nixos-generate-config --no-filesystems --show-hardware-config >> ./hosts/$1/hardware-configuration.nix
+nixos-generate-config --no-filesystems --root /tmp
+mv /tmp/etc/nixos/hardware-configuration.nix ./hosts/$1/
+rm -rf /tmp/etc
 git add ./hosts/$1/hardware-configuration.nix
 sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#"$1" --disk main "$2"
