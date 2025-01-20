77baf5d81aafdb3d774413047a185af24c4b41a4
RadioAddition <radioaddition@pm.me>
2025-01-20 02:49:34 -0500

    fix incorrect device reference

--- init/filesystem.nix
+++ init/filesystem.nix
@@ -7,7 +7,7 @@
 
   boot.initrd.postResumeCommands = lib.mkAfter ''
     mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
+    mount /dev/mapper/crypted /btrfs_tmp
     if [[ -e /btrfs_tmp/root ]]; then
         mkdir -p /btrfs_tmp/old_roots
         timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
