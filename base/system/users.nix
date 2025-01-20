90f35581a0b7d506ba7abc186e4d143376d306f4
RadioAddition <radioaddition@pm.me>
2025-01-20 02:33:55 -0500

    temporary fix to broken run0

--- base/system/users.nix
+++ base/system/users.nix
@@ -1,16 +1,16 @@
 { config, pkgs, inputs, lib, ... }: {
 # Create a separate wheel user
-  users.users.radioadmin = {
-    isNormalUser = true;
-    description = "RadioAdmin";
-    extraGroups = [ "adbusers" "docker" "kvm" "libvirt" "libvirtd" "lxd" "networkmanager" "openrazer" "wheel" "nix" ];
-    # Change this password on an actual system!!
-    hashedPassword = "$y$j9T$gMRIRcus7uO1X6zrPTfVn/$0iOFINi8HZPH5b0QpXXCQbanUwYe9lpzjD17NbitD39";
-  };
+#  users.users.radioadmin = { # This has to be disabled until I can get building with run0 to work properly
+#    isNormalUser = true;
+#    description = "RadioAdmin";
+#    extraGroups = [ "adbusers" "docker" "kvm" "libvirt" "libvirtd" "lxd" "networkmanager" "openrazer" "wheel" "nix" ];
+#    # Change this password on an actual system!!
+#    hashedPassword = "$y$j9T$gMRIRcus7uO1X6zrPTfVn/$0iOFINi8HZPH5b0QpXXCQbanUwYe9lpzjD17NbitD39";
+#  };
   users.users.radioaddition = {
     isNormalUser = true;
     description = "RadioAddition";
-    extraGroups = [ "adbusers" "docker" "kvm" "libvirt" "libvirtd" "lxd" "networkmanager" "openrazer" "nix" ];
+    extraGroups = [ "adbusers" "docker" "kvm" "libvirt" "libvirtd" "lxd" "networkmanager" "openrazer" "wheel" "nix" ];
     # Change this password on an actual system!!
     hashedPassword = "$y$j9T$gMRIRcus7uO1X6zrPTfVn/$0iOFINi8HZPH5b0QpXXCQbanUwYe9lpzjD17NbitD39";
   };
