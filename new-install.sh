3cdb00567d4cc20d5e08e14016dc3e068b6ded80
RadioAddition <radioaddition@pm.me>
2025-01-19 21:17:58 -0500

    modify install script to use installation flake

--- new-install.sh
+++ new-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ "$2" == "" ]; then
-	echo Usage: ./new-install.sh [hostname] [disk device]
+if [ "$1" == "" ]; then
+	echo Usage: ./new-install.sh [disk device]
 	exit 1
 fi
 nixos-generate-config --no-filesystems --root /tmp
-mv /tmp/etc/nixos/hardware-configuration.nix ./hosts/$1/
+mv /tmp/etc/nixos/hardware-configuration.nix ./hosts/installer/
 rm -rf /tmp/etc
-git add ./hosts/$1/hardware-configuration.nix
-sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#"$1" --disk main "$2"
+git add ./hosts/installer/hardware-configuration.nix
+sudo nix --extra-experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake .#installer --disk main "$1"
