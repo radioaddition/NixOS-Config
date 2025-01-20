1ff294b8931e63a37166ea8710918021457eab7d
RadioAddition <radioaddition@proton.me>
2025-01-19 19:01:03 -0500

    Previous commit cont'd

--- base/system/system.nix
+++ base/system/system.nix
@@ -17,6 +17,18 @@
     randomizedDelaySec = "45min";
   };
 
+  # Swapfile oneshot service
+  systemd.services = {
+    create-swapfile = {
+      serviceConfig.Type = "oneshot";
+      wantedBy = [ "swap-swapfile.swap" ];
+      script = ''
+        ${pkgs.coreutils}/bin/truncate -s 0 /swap/swapfile
+        ${pkgs.e2fsprogs}/bin/chattr +C /swap/swapfile
+        ${pkgs.btrfs-progs}/bin/btrfs property set /swap/swapfile compression none
+      '';
+    };
+  };
   ## Bootloader
   boot = {
 
