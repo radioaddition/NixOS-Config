031763bc557224cacfae778a87864274ee600ca3
RadioAddition <radioaddition@pm.me>
2025-01-20 02:22:44 -0500

    fix duplicate definition

--- base/system/security.nix
+++ base/system/security.nix
@@ -61,9 +61,6 @@
 
   security.virtualisation.flushL1DataCache = "always";
 
-  security.apparmor.enable = true;
-  security.apparmor.killUnconfinedConfinables = true;
-
   boot.kernelParams = [
     # Don't merge slabs
     "slab_nomerge"
