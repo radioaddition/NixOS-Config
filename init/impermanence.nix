9d5e6764a604e4acc08d785078521bd2f043de3e
RadioAddition <radioaddition@pm.me>
2025-01-22 14:21:35 -0500

    change how impermanence handles my homedir

--- init/impermanence.nix
+++ init/impermanence.nix
@@ -8,6 +8,7 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
+      { directory = "/home/radioaddition"; user = "radioaddition"; group = "radioaddition"; mode = "u=rw,g=r,o="; }
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
@@ -15,26 +16,26 @@
       #{ file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
   };
-  environment.persistence."/home" = {
-    enable = true;
-    hideMounts = true;
-    users.radioaddition = {
-      directories = [
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/direnv"
-	".config"
-	".local"
-      ];
-      files = [
-      ];
-    };
-  };
+  #environment.persistence."/home" = {
+  #  enable = true;
+  #  hideMounts = true;
+  #  users.radioaddition = {
+  #    directories = [
+  #      "Downloads"
+  #      "Music"
+  #      "Pictures"
+  #      "Documents"
+  #      "Videos"
+  #      { directory = ".gnupg"; mode = "0700"; }
+  #      { directory = ".ssh"; mode = "0700"; }
+  #      { directory = ".nixops"; mode = "0700"; }
+  #      { directory = ".local/share/keyrings"; mode = "0700"; }
+  #      ".local/share/direnv"
+  #      ".config"
+  #      ".local"
+  #    ];
+  #    files = [
+  #    ];
+  #  };
+  #};
 }
