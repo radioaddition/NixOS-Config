9a2d808d1d868e38240a2c9cc96f548768964f5a
RadioAddition <radioaddition@pm.me>
2025-01-22 10:44:21 -0500

    fix home-manager service timeout

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -1,4 +1,6 @@
 { config, pkgs, inputs, lib, ... }: {
   # Define system hostname
   networking.hostName = "framework";
+  # Extend timeout of home-manager service so it doesn't fail
+  systemd.services.home-manager-radioaddition.serviceConfig.TimeoutStartSec = lib.mkForce 600;
 }
