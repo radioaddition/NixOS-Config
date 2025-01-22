8e68ed3a159317f44226b1eee22d121f073acab3
RadioAddition <radioaddition@pm.me>
2025-01-22 15:34:26 -0500

    make impermanence ignore the home subvolume

--- init/impermanence.nix
+++ init/impermanence.nix
@@ -8,7 +8,6 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      { directory = "/home/radioaddition"; user = "radioaddition"; group = "radioaddition"; mode = "u=rw,g=r,o="; }
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
