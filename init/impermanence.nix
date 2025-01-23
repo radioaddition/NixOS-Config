72fcaf0baa3d37b8b9cf0a892c048806f6d04f2e
RadioAddition <radioaddition@pm.me>
2025-01-23 00:46:05 -0500

    fix directory marked as file

--- init/impermanence.nix
+++ init/impermanence.nix
@@ -8,11 +8,11 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
+      "/etc/secureboot"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
       "/var/lib/usbguard/rules.conf"
-      "/etc/secureboot"
       #{ file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
   };
