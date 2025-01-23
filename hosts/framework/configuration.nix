6d4ad3d79bb58907ebbdb472a1381724d2bbba89
RadioAddition <radioaddition@pm.me>
2025-01-22 22:38:58 -0500

    fingerprint reader support on framework

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -1,6 +1,16 @@
 { config, pkgs, inputs, lib, ... }: {
   # Define system hostname
   networking.hostName = "framework";
+
   # Extend timeout of home-manager service so it doesn't fail
   systemd.services.home-manager-radioaddition.serviceConfig.TimeoutStartSec = lib.mkForce 600;
+
+  # Enable fingerprint reader support
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix
+    };
+  };
 }
