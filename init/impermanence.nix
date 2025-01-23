0b67f4549b81ab5ce7daccd0ef14f47fe7cf12a2
RadioAddition <radioaddition@pm.me>
2025-01-22 22:29:40 -0500

    persist usbguard policy file

--- init/impermanence.nix
+++ init/impermanence.nix
@@ -11,30 +11,8 @@
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
-      #"/etc/machine-id"
+      "/var/lib/usbguard/rules.conf"
       #{ file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
   };
-  #environment.persistence."/home" = {
-  #  enable = true;
-  #  hideMounts = true;
-  #  users.radioaddition = {
-  #    directories = [
-  #      "Downloads"
-  #      "Music"
-  #      "Pictures"
-  #      "Documents"
-  #      "Videos"
-  #      { directory = ".gnupg"; mode = "0700"; }
-  #      { directory = ".ssh"; mode = "0700"; }
-  #      { directory = ".nixops"; mode = "0700"; }
-  #      { directory = ".local/share/keyrings"; mode = "0700"; }
-  #      ".local/share/direnv"
-  #      ".config"
-  #      ".local"
-  #    ];
-  #    files = [
-  #    ];
-  #  };
-  #};
 }
