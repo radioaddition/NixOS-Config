c6c4c8605600bab29b966ebea72843cac5e57bb4
RadioAddition <radioaddition@pm.me>
2025-01-22 20:20:05 -0500

    remove dnscache

--- base/system/networking.nix
+++ base/system/networking.nix
@@ -4,7 +4,6 @@
   #- networking.proxy.default = "http://user:password@proxy:port/";
   #- networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  services.dnscache.enable = true;
   services.coredns.enable = true;
 
   ### Configure NetworkManager
